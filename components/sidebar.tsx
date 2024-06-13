056d955d5e74c07a36a009e402e64f0f394f9442
Bal Kishan <bkunsolved@gmail.com>
2024-06-13 18:28:41 +0530

    first commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SidebarItem from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
+type Props = {
+  className?: string;
+};
+
+function Sidebar({ className }: Props) {
+  return (
+    <div
+      className={cn(
+        "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/images/logo.png"} height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" />
+        <SidebarItem label="Leaderboard" href="/leaderboard" />
+        <SidebarItem label="Quest" href="/quest" />
+        <SidebarItem label="Shop" href="/shop" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
